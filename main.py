cc2215f9091779bf2fe0abc80ac5928ba4c7ecdb
ExoticGamerrrYT <carlosalfaromolina27@gmail.com>
2023-05-31 19:37:53 +0200

    clock in!

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from pygame import *
 
+# Constants
+FPS = 60
+
+# Main settings
 window = display.set_mode((700, 500))
 display.set_caption("Caught")
-background = transform.scale(image.load("images/08-05-23 19.png"), (700, 500))
+clock = time.Clock()
 
-window.blit(background, (0, 0))
+# Images
+background = transform.scale(image.load("images/08-05-23 19.png"), (700, 500))
+tomato = transform.scale(image.load("images/tomate.png"), (200, 150))
+
+# Game cycle
+game = True
+while game:
+    window.blit(background, (0, 0))
+    window.blit(image.load("images/16-05-23 23.png"), (1, 0))
+    window.blit(tomato, (500, 0))
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    clock.tick(FPS)
